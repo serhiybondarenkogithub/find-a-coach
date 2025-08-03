07096f22cfcdfc6e7cbeb930d0cffd248fb5d63b
serhiybondarenkogithub <sergiogetstudy@gmail.com>
2025-08-03 03:01:16 +0300

    added route page components

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import CoachDetails from "./pages/coaches/CoachDetails.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import RequestReceived from "./pages/requests/RequestReceived.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/coaches" },
-    { path: "/coaches", component: null },
+    { path: "/coaches", component: CoachesList },
     {
       path: "coaches/:id",
-      component: null,
-      Children: [{ path: "contact", component: null }],
+      component: CoachDetails,
+      Children: [{ path: "contact", component: ContactCoach }],
     },
-    { path: "/register", component: null },
-    { path: "request", component: null },
-    { path: ":notFound(.*)", component: null },
+    { path: "/register", component: CoachRegistration },
+    { path: "request", component: RequestReceived },
+    { path: ":notFound(.*)", component: NotFound },
   ],
 });
 
